1c9719ea720e5e678bfa046f417dc56b8a366a6d
yidan98 <sonngeitann@gmail.com>
2022-07-21 08:55:13 +0900

    todoList

---
+++ src/components/todo-item/todoItem.component.jsx
@@ -0,0 +1,22 @@
+const TodoItem = ({ item, updateStatus, deletePost }) => {
+  const { task, date, status } = item;
+  return (
+    <div className="todo-item">
+      <div className="item-content">
+        <p style={{ textDecoration: status !== 1 ? "line-through" : "none" }}>
+          {task}
+        </p>
+        <span>{date}</span>
+      </div>
+      <div className="todo-button">
+        <button onClick={() => updateStatus(item)}>toggle</button>
+        <button onClick={() => deletePost(item)}>remove</button>
+      </div>
+    </div>
+  );
+};
+
+// <span>(id:{taskId})</span>
+// <span>(status:{status})</span>
+
+export default TodoItem;
---
+++ src/components/todo-list/todoList.component.jsx
@@ -0,0 +1,45 @@
+import TodoItem from "../todo-item/todoItem.component";
+import axios from "axios";
+const todoUrl = axios.create({
+  baseURL: "http://localhost:8080/todo",
+});
+const TodoList = ({ items, setTodoList }) => {
+  function updateStatus(item) {
+    axios
+      .patch(`http://localhost:8080/todo/${item.taskId}`, {
+        status: item.status === 1 ? 0 : 1,
+      })
+      .then((response) => {
+        setTodoList(response.data.data);
+      });
+  }
+  async function deletePost(item) {
+    if (window.confirm("确认删除吗？")) {
+      const response = await todoUrl.delete(`/${item.taskId}`);
+
+      setTodoList(response.data.data);
+    }
+  }
+  const handleClick = () => {
+    items.sort((a, b) => new Date(a.date) - new Date(b.date));
+    setTodoList([...items]);
+    console.log("items", items);
+  };
+  return (
+    <div className="todo-list">
+      <button onClick={handleClick}>Sort by date</button>
+      {items.map((item) => {
+        return (
+          <TodoItem
+            key={item.taskId}
+            item={item}
+            updateStatus={() => updateStatus(item)}
+            deletePost={() => deletePost(item)}
+          ></TodoItem>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
